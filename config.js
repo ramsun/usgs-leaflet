5ee4b49e3153bdc78c1ccc1f612dfd9079d5ab88
Ramamurthy Sundar <rsramamurthysundar@gmail.com>
2019-06-07 23:13:45 -0400

    Initial upload

---
+++ config.js
@@ -0,0 +1,1 @@
+const API_KEY = "pk.eyJ1IjoicmFtc3VuIiwiYSI6ImNqd214YXlkeDFqa3IzeXA5OHpwejZxcmUifQ.2Q1bNA_M6U6lQRTWLZAitg";
---
+++ logic.js
@@ -0,0 +1,3 @@
+// Store the USGS JSON url
+var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
